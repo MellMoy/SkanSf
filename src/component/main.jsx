ce892adb3189b7e16086a6e96e478cc0846dfa1e
MellMoy <swaggadanil@mail.ru>
2024-11-09 13:44:24 +0700

    формления майн

--- src/component/main.jsx
+++ src/component/main.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import mainlogo from "../media/mainlogo.svg";
 
-
-
-
-
-
-
-
-export default function Footer() {
+export default function Main() {
   return (
     <main>
-
+      <div class="container">
+        <div class="text-content">
+          <h1>СЕРВИС ПО ПОИСКУ ПУБЛИКАЦИЙ <br/>О КОМПАНИИ <br/>ПО ЕГО ИНН</h1>
+          <p>
+            Комплексный анализ публикаций, получение данных <br/>в формате PDF на
+            электронную почту.
+          </p>
+          <button>Запросить данные</button>
+        </div>
+        <div class="image-content">
+          <img alt="Illustration of a persons" src={mainlogo}/>
+        </div>
+      </div>
     </main>
   );
 }
